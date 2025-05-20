e30e4cf81822a01c9d9d4c68a656701a0db72ff4
Jérémie Banier <jbanier@users.noreply.github.com>
2025-05-20 22:48:01 +0200

    feat: insert workunit

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,24 @@
 pub mod models;
 pub mod schema;
+
+use diesel::prelude::*;
+use dotenvy::dotenv;
+use models::*;
+use std::env;
+
+pub fn establish_connection() -> SqliteConnection {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    SqliteConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+}
+
+pub fn create_work_unit(conn: &mut SqliteConnection, work_unit: &WorkUnit) -> WorkUnit{
+    use crate::schema::WorkUnit;
+
+    diesel::insert_into(WorkUnit::table)
+        .values(work_unit)
+        .get_result(conn)
+        .expect("Error saving new post")
+}
